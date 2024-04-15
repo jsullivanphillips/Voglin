3934548532ae8e414bca202b465ad7b5233c2ebc
Jamie Sullivan-Phillips <47346975+jsullivanphillips@users.noreply.github.com>
2024-04-15 12:06:03 -0700

    basic player movement and collision with environment

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    
+    [SerializeField] PlayerMovement _PlayerMovement;
+
+    private void Update() 
+    {
+         MovementInput();
+    }
+
+    private void MovementInput()
+    {
+        float inputHorizontal = 0;
+        float inputVertical = 0;
+
+        bool up = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+        bool down = Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+        bool left = Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+        bool right = Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+
+        if (up ^ down) // XOR operator, true if either up or down is true, but not both
+        {
+            inputVertical = up ? 1 : -1;
+        }
+
+        if (left ^ right) // XOR operator, true if either left or right is true, but not both
+        {
+            inputHorizontal = right ? 1 : -1;
+        }
+
+        Vector2 inputVector = new Vector2(inputHorizontal, inputVertical);  
+
+        _PlayerMovement.MovePlayer(inputVector);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float speed = 5f;
+    [SerializeField] BoxCollider2D _BoxCollider2D;
+    [Tooltip("The layers that the player should collide with.")]
+    [SerializeField] LayerMask _CollisionLayerMask;
+
+    public void MovePlayer(Vector2 inputVector)
+    {
+        #region Preparing input
+        if (inputVector.magnitude > 1)
+        {
+            inputVector = inputVector.normalized;
+        }
+
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+
+        Vector2 horizontalMovement = new Vector2(inputVector.x, 0) * speed * Time.deltaTime;
+        Vector2 verticalMovement = new Vector2(0, inputVector.y) * speed * Time.deltaTime;
+        #endregion
+
+        #region Collision filters
+        // Create a ContactFilter2D to ignore the player's own collider
+        ContactFilter2D contactFilter = new ContactFilter2D();
+        contactFilter.SetLayerMask(_CollisionLayerMask);
+        #endregion
+
+        #region Horizontal collision detection & movement
+        // Check if the new horizontal position would be inside a collider
+        List<Collider2D> collisions = new List<Collider2D>();
+        Physics2D.OverlapBox(currentPosition + horizontalMovement, _BoxCollider2D.size, 0f, contactFilter, collisions);
+       
+
+        // If the new horizontal position is not inside a collider, move the player horizontally
+        if (collisions.Count == 0)
+        {
+            currentPosition += horizontalMovement;
+        }
+        #endregion
+
+        // Clear the collisions list
+        collisions.Clear();
+
+        #region Vertical collision detection & movement
+        // Check if the new vertical position would be inside a collider
+        Physics2D.OverlapBox(currentPosition + verticalMovement, _BoxCollider2D.size, 0f, contactFilter, collisions);
+
+        // If the new vertical position is not inside a collider, move the player vertically
+        if (collisions.Count == 0)
+        {
+            currentPosition += verticalMovement;
+        }
+        #endregion
+
+        transform.position = new Vector3(currentPosition.x, currentPosition.y, 0f);
+    }
+    
+}
